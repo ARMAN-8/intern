fe9496a221f180c693db15de49cb3a9e7d4ab1c0
RAZOR <alimnurarman@gmail.com>
2021-03-16 14:26:22 +0600

    little fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="home h-screen">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <NavBar/>
+            <navBar/>
             <div class="home-content mt-16">
                 <div class="grid grid-flow-cols-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-content-center my-8">
-                    <div class="text-white">
+                    <div class="">
                         <div class="text-4xl font-extrabold">
-                            home
                             {{ main.title }}
                         </div>
                         <div class="mt-8">
@@ -16,11 +15,6 @@
                             {{ main.paragraph_2 }}
                         </div>
                         <div class="mt-8">
-                            <!--<ul id="list-rendering">
-                                <li v-for="todo in todos">
-                                    {{ todo.text }}
-                                </li>
-                            </ul>-->
                         </div>
                     </div>
                     <div>
@@ -33,12 +27,12 @@
 </template>
 
 <script>
-    import NavBar from "../components/layouts/NavBar";
+    import navBar from "../components/navBar";
 
     export default {
         name: 'Home',
         components: {
-            NavBar
+            navBar
         },
         data() {
             return {
@@ -51,17 +45,4 @@
             }
         }
     }
-    /*const ListRendering = {
-        data() {
-            return {
-                todos: [{ message: 'Foo' }, { message: 'Bar' }]
-            }
-        }
-    };
-    Vue.createApp(ListRendering).mount('#list-rendering')*/
 </script>
-<style>
-    .home {
-        background-image: url('../assets/main-bg.jpg');
-    }
-</style>
