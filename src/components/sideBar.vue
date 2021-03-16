33f68dfda25b0390c05e9412e113e9bbeed1a582
RAZOR <alimnurarman@gmail.com>
2021-03-16 14:23:04 +0600

    left side menu "sideBar" added to app

---
+++ src/components/sideBar.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="fixed">
+        <div class="sideBar min-h-screen">
+            <div>
+                <div class="flex items-center justify-center py-8">
+                    <img src="../assets/INTERN-TEST.png" alt="">
+                </div>
+                <div class="mt-12">
+                    <!--<div>
+                        <img src="../assets/active.png" alt="">
+                    </div>-->
+                    <ul class="m-8">
+                        <li class="flex items-center m-8">
+                            <a href="#" class="sideBarText">
+                                <img src="../assets/tests.png" alt="">
+                                <span class="ml-4">{{ content.link_1 }}</span>
+                            </a>
+                        </li>
+                        <li class="flex items-center m-8">
+                            <a href="#" class="sideBarText">
+                                <img src="../assets/tests.png" alt="">
+                                <span class="ml-4">{{ content.link_2 }}</span>
+                            </a>
+                        </li>
+                        <li class="flex items-center m-8">
+                            <a href="#" class="sideBarText">
+                                <img src="../assets/tests.png" alt="">
+                                <span class="ml-4">{{ content.link_3 }}</span>
+                            </a>
+                        </li>
+                        <li class="flex items-center m-8">
+                            <a href="#" class="sideBarText">
+                                <img src="../assets/tests.png" alt="">
+                                <span class="ml-4">{{ content.link_4 }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'sideBar',
+        data () {
+            return {
+                content: {
+                    link_1: 'Тесты',
+                    link_2: 'Тарифы',
+                    link_3: `Настройки аккаунта`,
+                    link_4: 'Обучение',
+                }
+            }
+        }
+    }
+</script>
+<style>
+    .sideBar {
+        width: 300px;
+        background: rgba(255, 204, 0, 1);
+    }
+    .sideBarText {
+        @apply px-2 font-medium font-medium text-xl flex items-center justify-center my-4 hover:text-gray-400 focus:text-white
+    }
+</style>
