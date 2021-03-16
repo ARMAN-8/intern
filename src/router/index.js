39b7c458ff721592236e13a76ec4958b545f8f5b
RAZOR <alimnurarman@gmail.com>
2021-03-16 14:25:56 +0600

    little fixes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -17,11 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  },
+  {
+    path: '/block',
+    name: 'Block',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Block.vue')
+  },
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
 export default router
