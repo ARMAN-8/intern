e9e509963d4f3f5572febdf030eaa931ff3d852a
RAZOR <alimnurarman@gmail.com>
2021-03-16 14:27:11 +0600

    new fixes

---
+++ src/components/continent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="lg:ml-80 md:ml-72 my-8 mx-8 px-8">
+            <mainContent/>
+        </div>
+    </div>
+</template>
+<script>
+    import mainContent from "./mainContent";
+
+    export default {
+        name: 'continent',
+        components: {
+            mainContent
+        }
+    }
+</script>
---
+++ src/views/Block.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Block">
+        <sideBar/>
+        <mainNavBar/>
+        <continent/>
+    </div>
+</template>
+<script>
+    import sideBar from "../components/sideBar";
+    import mainNavBar from "../components/mainNavBar";
+    import continent from "../components/continent";
+
+    export default {
+        name: 'Block',
+        components: {
+            sideBar,
+            mainNavBar,
+            continent,
+        }
+    }
+</script>
