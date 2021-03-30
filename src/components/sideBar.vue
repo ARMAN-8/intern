4927b287b0b7d2d29b05960683455b84eccf2cd3
RAZOR <alimnurarman@gmail.com>
2021-03-30 17:59:37 +0600

    added router active-link

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed">
-        <div class="sideBar">
+        <div class="sideBar" id="sidebar">
             <div class="min-h-screen lg:w-80 md:w-0 sm:w-0">
                 <div class="flex items-center justify-center py-8">
                     <img src="../assets/INTERN-TEST.png" alt="">
@@ -9,27 +9,28 @@
                     <!--<div>
                         <img src="../assets/active.png" alt="">
                     </div>-->
-                    <ul class="m-8">
-                        <li class="sideBarMenu">
-                            <router-link class="sideBarText" to="/Block" v-bind:class="{ active: isActive }">
-                                <img src="../assets/tests.png" alt="">
+                    <ul class="bor">
+                        <li class="sideBarMenu m-8 bor">
+                            <router-link class="sideBarText" exact to="/Block">
+                                <!--<img src="../assets/tests.png" alt="">-->
+                                <i class="fas fa-shopping-cart"/>
                                 <span class="ml-4">{{ content.link_1 }}</span>
                             </router-link>
                         </li>
                         <li class="sideBarMenu">
-                            <router-link class="sideBarText" to="/tariffs" v-bind:class="{ active: isActive }">
+                            <router-link class="sideBarText" exact to="/tariffs">
                                 <img src="../assets/tests.png" alt="">
                                 <span class="ml-4">{{ content.link_2 }}</span>
                             </router-link>
                         </li>
                         <li class="sideBarMenu">
-                            <router-link class="sideBarText" to="/account-settings" v-bind:class="{ active: isActive }">
+                            <router-link class="sideBarText" exact to="/account-settings">
                                 <img src="../assets/tests.png" alt="">
                                 <span class="ml-4">{{ content.link_3 }}</span>
                             </router-link>
                         </li>
                         <li class="sideBarMenu">
-                            <router-link class="sideBarText" to="/video-lessons" v-bind:class="{ active: isActive }">
+                            <router-link class="sideBarText" exact to="/video-lessons">
                                 <img src="../assets/tests.png" alt="">
                                 <span class="ml-4">{{ content.link_4 }}</span>
                             </router-link>
@@ -60,10 +61,16 @@
         background: rgba(255, 204, 0, 1);
     }
     .sideBarText {
-        @apply px-2 font-medium font-medium text-xl flex items-center justify-center my-4 hover:text-gray-400 focus:text-white
+        @apply px-2 font-medium text-lg flex items-center justify-center my-4 hover:text-gray-400 focus:text-white
     }
     .sideBarMenu {
         color: black;
-        @apply  flex items-center m-8 w-full
+        @apply  flex items-center m-8 w-max ml-0
+    }
+    #sidebar a {
+        color: #000;
+    }
+    #sidebar a.router-link-active {
+        color: #fff;
     }
 </style>
