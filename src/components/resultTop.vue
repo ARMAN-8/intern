93726b80c8d5e0397b10c7965ecaf6c650b9cfb0
RAZOR <alimnurarman@gmail.com>
2021-04-07 14:25:05 +0600

    added result menu

---
+++ src/components/resultTop.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mx-8 my-8">
+        <div class="flex justify-between">
+            <div>
+                <img src="../assets/i_test_logo.png" class="h-12 w-auto" alt=""/>
+            </div>
+            <div class="font-extrabold lg:text-2xl md:text-2xl text-xl">
+                <router-link to="/tests">{{ backToPage }}</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'resultTop',
+        data() {
+            return {
+                backToPage: `Назад`,
+            }
+        }
+    }
+</script>
